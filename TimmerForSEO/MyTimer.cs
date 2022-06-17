4d82d0df276fecac59faeda9f2b017f1b7c92957
Dric77 <92301957+Tornike-meama@users.noreply.github.com>
2022-06-17 22:12:10 +0400

    ssd

---
+++ TimmerForSEO/MyTimer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimmerForSEO
+{
+    internal class MyTimer
+    {
+        public int CurrentTime { get; private set; }
+        public bool IsStartTimer { get; set; } = false;
+        public bool IsStopTimer { get; set; } = false;
+
+        public delegate void TimerEvent();
+
+        public static event TimerEvent StartTimerEvet;
+
+        public void StartTimer()
+        {
+            IsStartTimer = true;
+            Task timer = new Task(() => Timer());
+            timer.Start();
+
+            Console.WriteLine("press any key  and enter  pause Timer");
+
+            Console.ReadLine();
+
+            PauseTimer();
+
+        }
+
+        private void Timer()
+        {
+           while(IsStartTimer)
+            {
+                Thread.Sleep(1000);
+                StartTimerEvet();
+                IsStopTimer = false;
+                CurrentTime++;
+            }
+        }
+
+        //Task traffTask = new Task(() => TrafficLights());
+        //traffTask.Start();
+        public void StopTimer()
+        {
+            IsStopTimer = true;
+            Console.WriteLine("Stop Timer");
+        }
+
+        public void PauseTimer()
+        {
+            IsStopTimer = true;
+            IsStartTimer = false;
+            Console.WriteLine("Pause Timer");
+
+            Console.WriteLine("Press any key and enter resume Timer");
+            Console.ReadLine();
+            StartTimer();
+        }
+    }
+}
---
+++ TimmerForSEO/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace TimmerForSEO
+{
+    internal class Program
+    {
+        static MyTimer myTimer;
+        static void Main(string[] args)
+        {
+            myTimer = new MyTimer();
+            MyTimer.StartTimerEvet += TestTimer;
+            myTimer.StartTimer();
+
+            Console.ReadKey();
+        }
+
+
+        public static void TestTimer()
+        {
+            Console.WriteLine($"timee {myTimer.CurrentTime}");
+        }
+
+    }
+}
